a5659366ced3df931b211818e6b2b27b059521be
Raju <rajdeepsrivastava2009@gmail.com>
2026-01-20 18:52:27 +0530

    Add files via upload

---
+++ Editor/Save/NodeSaveData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS.Data.Save
+{
+    [System.Serializable]
+    public class NodeSaveData
+    {
+        public string ID { get; set; }
+        public string Dialogue { get; set; }
+        public List<ChoicesSaveData> Choices { get; set; }
+        public string GroupID { get; set; }
+        public Vector2 Position { get; set; }
+    }
+}
